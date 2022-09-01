5d515b5b4c06bb59b87499409adfe18d9093a816
mazurenka <an.drej@mail.ru>
2022-09-01 08:51:19 +0200

    03

--- src/03/03.ts
+++ src/03/03.ts
@@ -1,3 +1,4 @@
+debugger
 export const sum = (a: number, b: number) => {
     return a + b
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {sum} from "./01/01";
+
+var a = sum
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
