2a69fea7b101a1daa369ef33b7755ad3f6fda91b
mazurenka <an.drej@mail.ru>
2022-10-28 12:44:08 +0200

    07

---
+++ src/07/07.test.ts
@@ -0,0 +1,20 @@
+test('', () => {
+
+    let props = {
+        name: 'Andre',
+        age: 40,
+        lessons: [{title: 1}, {title: 2}],
+    }
+
+    //let age = props.age
+    //let lessons = props.lessons
+    let {age, lessons} = props
+
+    let a = props.age
+    let l = props.lessons
+
+    expect(age).toBe(40)
+    expect(lessons.length).toBe(2)
+    expect(a).toBe(40)
+    expect(l.length).toBe(2)
+})
