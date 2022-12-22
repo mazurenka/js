d00bb5f9eb9237a17cd0a237cc5ca567f002bb5c
mazurenka <an.drej@mail.ru>
2022-12-22 11:58:40 +0100

    08

--- src/08/08.ts
+++ src/08/08.ts
@@ -5,9 +5,25 @@ export const usersObj = {
     '3': 'Katya',
 }
 
-export const users = {
+type UsersType = {
+    [key: string]: { id: number, name: string }
+}
+
+export const users: UsersType = {
     '0': {id: 101, name: 'Dimych'},
     '1': {id: 3232312, name: 'Dimych'},
     '2': {id: 1212, name: 'Dimych'},
     '3': {id: 1, name: 'Dimych'},
 }
+//users[1].id
+var user = {id: 100500, name: 'Igor'}
+users[user.id.toString()] = user
+
+export const usersArray = [
+    {id: 101, name: 'Dimych'},
+    {id: 3232312, name: 'Dimych'},
+    {id: 1212, name: 'Dimych'},
+    {id: 1, name: 'Dimych'},
+]
+
+usersArray.find(u => u.id === 1)
