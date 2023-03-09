082e183b4adfbdf9261280d186ab56676502df13
mazurenka <an.drej@mail.ru>
2023-03-09 12:12:19 +0100

    09

--- src/09/09.test.ts
+++ src/09/09.test.ts
@@ -7,7 +7,7 @@ function incraseAge(u: UserType) {
     u.age++ //u.age = u.age + 1
 }
 
-test('Big test', () => {
+test('reference type test', () => {
 
     let user = {
         name: "Andre",
@@ -23,6 +23,24 @@ test('Big test', () => {
     superman.age = 1000
 
     expect(user.age).toBe(1000)
+})
 
+test('array reference test', () => {
 
+    let users = [
+        {
+            name: "Andre",
+            age: 15
+        },
+        {
+            name: "Igor",
+            age: 11
+        }
+    ]
+
+    let admins = users
+
+    admins.push({name: "Sarah", age: 19})
+
+    expect(users[2]).toEqual({name: "Sarah", age: 19})
 })
