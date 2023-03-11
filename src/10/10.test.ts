5f7bb8ca5bad815b24a12179c196c6180c439dad
mazurenka <an.drej@mail.ru>
2023-03-11 19:35:42 +0100

    10

---
+++ src/10/10.test.ts
@@ -0,0 +1,24 @@
+import {UserType} from "./10";
+
+function hairdresser(u: UserType, power: number) {
+    const userCopy = {
+        ...u
+    }
+    userCopy.hair = u.hair / power
+    return userCopy
+}
+
+test("reference type test", () => {
+    let user: UserType = {
+        name: "Andre",
+        hair: 45,
+        address: {
+            title: "NY"
+        }
+    }
+
+    const cutUser = hairdresser(user, 2)
+
+    expect(cutUser.hair).toBe(22.5)
+    expect(user.hair).toBe(45)
+})
